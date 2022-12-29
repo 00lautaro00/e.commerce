915c6af40501381d0788755e7e7317b04e1ea64c
00lautaro00 <00loboi00@gmail.comm>
2022-12-28 23:15:27 -0300

    add carousel

--- src/functions/render.js
+++ src/functions/render.js
@@ -3,33 +3,43 @@
 export const renderCarrousel = async (data, carrouselImage) => {
     
 
-    const images = await data ?  data.results.slice(4,7) : [];
-     images.map(imgs => {
-        // const div = document.createElement("div");
-        //  div.classList.add("carousel-item");
-        //  div.classList.add("active");
-        // carrouselImage.append(div);
-        // div.innerHTML = `<img src="${imgs.thumbnail}" class="d-block w-20"  alt="...">`
-    });
-}
-
-
-export const renderProductCart = async (data, cardProduct) => {
-
-    const images = await data ?  data.results.slice(0,8) : [];
-    
+    const images = await data ?  data.results.slice(0,9) : [];
      images.map(imgs => {
         const a = document.createElement("a");
         const div = document.createElement("div");
         a.classList.add("product")
         div.classList.add("card");
         div.classList.add("w-product");
-        cardProduct.append(a)
-        a.append(div);
-        div.innerHTML = `<img src="${imgs.thumbnail}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <p class="card-text"> ${imgs.title.slice(0,18)}</p>
-                        <span class="prices">$${imgs.price}</span>
-                        </div>`
+         div.classList.add("item-slide");
+        carrouselImage.append(div);
+        div.innerHTML = 
+        `
+        <img src="${imgs.thumbnail}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text"> ${imgs.title.slice(0,18)}</p>
+        <span class="prices">$${imgs.price}</span>
+        </div>
+        `
     });
-} 
+}
+
+
+// export const renderProductCart = async (data, cardProduct) => {
+
+//     const images = await data ?  data.results.slice(0,8) : [];
+    
+//      images.map(imgs => {
+//         const a = document.createElement("a");
+//         const div = document.createElement("div");
+//         a.classList.add("product")
+//         div.classList.add("card");
+//         div.classList.add("w-product");
+//         cardProduct.append(a)
+//         a.append(div);
+//         div.innerHTML = `<img src="${imgs.thumbnail}" class="card-img-top" alt="...">
+//                         <div class="card-body">
+//                         <p class="card-text"> ${imgs.title.slice(0,18)}</p>
+//                         <span class="prices">$${imgs.price}</span>
+//                         </div>`
+//     });
+// } 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { renderCarrousel, renderProductCart } from "./functions/render.js"
-import { cardProduct, carrouselImage } from "./utils/dom.js"
+import { renderCarrousel} from "./functions/render.js"
+import {  carrouselImage } from "./utils/dom.js"
 
 export const init = async (data) => {
     const carrouselInit = await renderCarrousel(data, carrouselImage);
-    const cardProductInit = await renderProductCart(data, cardProduct);
+    // const cardProductInit = await renderProductCart(data, cardProduct);
 }
 
 init()
